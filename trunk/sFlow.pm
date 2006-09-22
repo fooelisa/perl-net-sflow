6c3a9ff8aaa0b59197a8638cffa005b03461561f
elisa <elisa@f0c0b545-b121-db11-b787-00e081619514>
2006-09-22 16:32:37 +0000

    * typo - 1 vergessen bei for schleife
    git-svn-id: svn+ssh://jasinska.de/usr/home/svn/perl-net-sflow@29 f0c0b545-b121-db11-b787-00e081619514

--- trunk/sFlow.pm
+++ trunk/sFlow.pm
@@ -170,7 +170,7 @@ sub decode {
     $offset += (3 * 4);
 
     # parse samples
-    for my $samplesCount (0 .. $sFlowDatagram{samplesInPacket}) {
+    for my $samplesCount (0 .. $sFlowDatagram{samplesInPacket} - 1) {
 
       my %sFlowSample = ();
       push @sFlowSamples, \%sFlowSample;
@@ -448,7 +448,7 @@ sub decode {
     $offset += 12;
 
     # parse samples
-    for my $samplesCount (0 .. $sFlowDatagram{samplesInPacket} -1) {
+    for my $samplesCount (0 .. $sFlowDatagram{samplesInPacket} - 1) {
 
       my %sFlowSample = ();
       push @sFlowSamples, \%sFlowSample;
