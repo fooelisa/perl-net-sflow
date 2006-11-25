09b7c35c0310bdafd826b16ada4c21a8ee088ba8
elisa <elisa@f0c0b545-b121-db11-b787-00e081619514>
2006-11-25 17:37:39 +0000

    branch 0.05 created
    git-svn-id: svn+ssh://jasinska.de/usr/home/svn/perl-net-sflow@38 f0c0b545-b121-db11-b787-00e081619514

---
+++ branches/0.05/test.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+#
+# test.pl for sFlow.pm
+#
+# Elisa Jasinska <elisa.jasinska@ams-ix.net>
+# 2006/11/8
+#
+
+use Test::More qw(no_plan);
+use Test::Pod;
+use Test::Pod::Coverage;
+
+BEGIN { use_ok( 'Net::sFlow' ); } 
+BEGIN { use_ok( 'Math::BigInt' ); }
+BEGIN { use_ok( 'Test::Pod' ); }
+
+pod_file_ok( 'blib/lib/Net/sFlow.pm', "Valid POD file" );
+pod_coverage_ok( "Net::sFlow", "Net::sFlow is covered" );
